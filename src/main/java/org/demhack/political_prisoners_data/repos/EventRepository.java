fe6aefeda953ab876420fc6bbd7f612354e58797
A. Ivanov <aleksandr.pv.ivanov@gmail.com>
2024-08-12 21:53:36 +0100

    EventRepository: find events by action type

--- src/main/java/org/demhack/political_prisoners_data/repos/EventRepository.java
+++ src/main/java/org/demhack/political_prisoners_data/repos/EventRepository.java
@@ -4,4 +4,5 @@ import org.demhack.political_prisoners_data.entities.Event;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface EventRepository extends MongoRepository<Event, String> {
+    Iterable<Event> findByAction(String action);
 }
